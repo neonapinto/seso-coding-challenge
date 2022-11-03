7d713cea3cedfd783d5f5409044373208f500480
Neona <neonapinto@gmail.com>
2022-11-03 00:46:05 -0400

    Added final solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -2,7 +2,67 @@
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
+const PriorityQueue = require('priorityqueuejs');
+
+// Print all entries, across all of the sources, in chronological order.
+
+module.exports = async (log_sources, printer) => {
+
+  //threshold to save some memory 
+  const THRESHOLD = 500;
+
+  //initialize the priority queue
+  var priority_queue = new PriorityQueue(function(a, b) {
+    if (a.item.date > b.item.date) return -1;
+    if (a.item.date < b.item.date) return 1;
+    return 0;
+  });
+
+  //conditions for enqueue log source 
+  const enqueueUtil = async (log_source) => {
+    //if log source is empty or crosses the threshold
+    if (log_source.drained || priority_queue.size() > THRESHOLD) {
+      return false;
+    }
+
+    const node = await log_source.popAsync();
+    //check if it exists
+    if (!node) {
+      return false;
+    }
+
+    //enqueue the log
+    priorityQueue.enq({
+      node: node,
+      log_source: log_source,
+    });
+
+    //if successful
+    return true;
+
+  }
+
+  //enqueue function to run all of enqueue asynchronously.  
+  await Promise.all(log_sources.map(async log_source => {
+    await enqueueUtil(log_source);
+  }));
+
+  //enqueue function to run all of enqueue  asynchronously.  
+  while (!priority_queue.isEmpty()) {
+    //dequeue the log with lowest date
+    const log_obj = priority_queue.deq();
+    //print the log
+    printer.print(log_obj.item);
+
+    //and then asynchronously enqueue it
+    await Promise.all(log_sources.map(async () => {
+      await enqueueUtil(log_obj.log_source);
+    }));
+  }
+
+  // print the stats
+  printer.done();
+
   return new Promise((resolve, reject) => {
     resolve(console.log("Async sort complete."));
   });
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,61 @@
 "use strict";
 
-// Print all entries, across all of the sources, in chronological order.
+//using priority queue to get a time complexity of O(nlogm)
 
-module.exports = (logSources, printer) => {
+const PriorityQueue = require('priorityqueuejs');
+
+// Print all entries, across all of the sources, in chronological order.
+module.exports = (log_sources, printer) => {
+  
+  //initialize the priority queue
+  var priorityQueue = new PriorityQueue(function(a, b) {
+    if (a.node.date > b.node.date) return -1;
+    if (a.node.date < b.node.date) return 1;
+    return 0;
+  });
+
+  //conditions for enqueue log source 
+  const enqueueUtil = (log_source) => {
+    //if logsources is empty
+    if (log_source.drained) {
+      return false;
+    }
+
+    const node = log_source.pop();
+
+    //check if log is undefined
+    if (!node) {
+      return false;
+    }
+
+    //Enqueue funtion to add into the priority queue 
+    priorityQueue.enq({
+      node: node,
+      log_source: log_source,
+    });
+
+    //if enqueuing is successful
+    return true;
+
+  }
+
+  //enqueue all log sources
+  for (const log_source of log_sources) {
+    enqueueUtil(log_source);
+  }
+
+  /**
+   * dequeue the highest priority and print it and 
+   * enqueue ref to the logsource
+   * **/
+
+  while (!priorityQueue.isEmpty()) {
+    const log_obj = priorityQueue.deq();
+    printer.print(log_obj.node);
+    enqueueUtil(log_obj.log_source);
+  }
+
+  // print the stats
+  printer.done();
   return console.log("Sync sort complete.");
 };
